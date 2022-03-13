383eb1a24829d3fca4b95c8088732f8135560973
emage <emage@a.b>
2022-03-13 13:12:13 +0400

    MUI LoginForm

--- src/components/LoginManager/LoginFormTS.tsx
+++ src/components/LoginManager/LoginFormTS.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import classes from './LoginForm.module.css';
 import {loginI18n} from "./LoginFormI18n";
+import {Box, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
 
 /**
  * Компонент с использованием PropTypes & DefaultProps (внизу пример).
@@ -28,14 +30,14 @@ interface LoginFormProps {
 }
 
 const LoginFormTS = ({
-                       isError = false,
-                       errorMessage,
-                       disabled = false,
-                       onLogin = f => console.log('onLogin: ', f),
-                       i18n = {},
-                       noForgotPassword = false,
-                       onForgotPassword = f => console.log('onForgotPassword: ', f),
-                   }: LoginFormProps) => {
+                         isError = false,
+                         errorMessage,
+                         disabled = false,
+                         onLogin = f => console.log('onLogin: ', f),
+                         i18n = {},
+                         noForgotPassword = false,
+                         onForgotPassword = f => console.log('onForgotPassword: ', f),
+                     }: LoginFormProps) => {
     // Слияние i18n
     i18n = {...loginI18n, ...i18n};
     // Хук react-hook-form
@@ -49,49 +51,48 @@ const LoginFormTS = ({
     //console.log('Identifier watch: ', watch('identifier')); // watch input value by passing the name of it
 
     return (
-        <div className={classes.loginForm}>
+        <div>
 
             {/*"handleSubmit" will validate your inputs before invoking "onSubmit"*/}
             <form onSubmit={handleSubmit(onSubmit)}>
                 {/*Заголовок, описание и ошибки*/}
-                <h1 className={classes.h1}>{i18n.title}</h1>
-                <div className={classes.description}>{i18n.description}</div>
-                <div className={classes.errorMessage}>
+                {/*<h1>{i18n.title}</h1>*/}
+                {/*<div>{i18n.description}</div>*/}
+                <div>
                     {isError && (errorMessage ?? i18n.errorMessage)}
                 </div>
+                <Box>
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <TextField label={i18n.identifier.title} variant="outlined" {...register('identifier', {
+                        required: i18n.identifier.required,
+                        minLength: {
+                            value: 2,
+                            message: i18n.identifier.validation
+                        },
+                        // validate: true
+                    })} />
+                    {/* errors will return when field validation fails  */}
+                    {errors.identifier && <p>{errors.identifier.message}</p>}
 
-                {/* include validation with required or other standard HTML validation rules */}
-                <label htmlFor="identifier">{i18n.identifier.title}</label>
-                <input type="text" {...register('identifier', {
-                    required: i18n.identifier.required,
-                    minLength: {
-                        value: 2,
-                        message: i18n.identifier.validation
-                    },
-                    // validate: true
-                })} />
-                {/* errors will return when field validation fails  */}
-                {errors.identifier && <p>{errors.identifier.message}</p>}
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <TextField label={i18n.password.title} type="password" variant="outlined" {...register('password', {
+                        required: i18n.password.required,
+                        minLength: {
+                            value: 8,
+                            message: i18n.password.validation
+                        },
+                        // validate: true
+                    })} />
+                    {/* errors will return when field validation fails  */}
+                    {errors.password && <p>{errors.password.message}</p>}
 
-                {/* include validation with required or other standard HTML validation rules */}
-                <label htmlFor="password">{i18n.password.title}</label>
-                <input type="password" {...register('password', {
-                    required: i18n.password.required,
-                    minLength: {
-                        value: 8,
-                        message: i18n.password.validation
-                    },
-                    // validate: true
-                })} />
-                {/* errors will return when field validation fails  */}
-                {errors.password && <p>{errors.password.message}</p>}
+                    {!noForgotPassword &&
+                        <a onClick={() => onForgotPassword({identifier: watch('identifier')})}>
+                            {i18n.forgotPassword}
+                        </a>}
 
-                {!noForgotPassword &&
-                    <a onClick={() => onForgotPassword({identifier: watch('identifier')})}>
-                        {i18n.forgotPassword}
-                    </a>}
-
-                <input disabled={disabled || !isValid} type="submit" value={i18n.submit}/>
+                    <input disabled={disabled || !isValid} type="submit" value={i18n.submit}/>
+                </Box>
             </form>
         </div>
     );
